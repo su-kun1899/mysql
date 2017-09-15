18ef25d8597744d4b84282c4b73bca2c7d126b23
su-kun1899 <higedrum.coz@gmail.com>
2017-09-15 15:50:15 +0900

    implements start script

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # mysql
 mysql 関連の置き場
+
+## dockerイメージの利用
+
+### Setup
+
+1. 公式からpullしてくる
+```
+docker pull mysql:5.6
+```
+
+### Run
+
+1. 必要に応じて起動Scriptのオプションを修正する。
+    - `docker/start_mysql_5_6.sh`
+1. 起動する
+    - `docker/start_mysql_5_6.sh`
+
+### Start/Stop
+
+1. dockerコマンドで実施
--- docker/start_mysql_5_6.sh
+++ docker/start_mysql_5_6.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
+# This script starts mysql docker container.
 
 set -eu
 
-echo "test"
+# Configuration
+readonly DATABASE_NAME="test"
+readonly PORT_NUMBER="43306"
+readonly CONTAINER_NAME="mysql-5-6"
+
+# start
+docker run \
+  --name ${CONTAINER_NAME} \
+  -p ${PORT_NUMBER}:3306 \
+  -e MYSQL_DATABASE=${DATABASE_NAME} \
+  -e MYSQL_ALLOW_EMPTY_PASSWORD=yes \
+  -d mysql:5.6
+
+echo "mysql docker container started."
+echo "    mysql -u root -h 127.0.0.1 -P ${PORT_NUMBER} -A ${DATABASE_NAME}"
 
 exit 0
