b6a2584a90bff2d3944c55110de11e6890052240
su-kun1899 <higedrum.coz@gmail.com>
2017-09-15 17:10:14 +0900

    Move config file
    docker$B$G$O%Q%9;XDj$N@_Dj$,$"$k$H$&$^$/F0:n$7$J$$(B
    docker$B@lMQ%U%!%$%k$H$7$F>l=j$r0\F0$7!"FI$_9~$`$h$&$K$7$?(B

--- docker/start_mysql_5_6.sh
+++ docker/start_mysql_5_6.sh
@@ -2,11 +2,13 @@
 # This script starts mysql docker container.
 
 set -eu
+cd `dirname $0`
 
 # Configuration
 readonly DATABASE_NAME="test"
 readonly PORT_NUMBER="43306"
 readonly CONTAINER_NAME="mysql-5-6"
+readonly CONF_DIRECTORY="$(pwd)/conf.d"
 
 # start
 docker run \
@@ -14,7 +16,9 @@ docker run \
   -p ${PORT_NUMBER}:3306 \
   -e MYSQL_DATABASE=${DATABASE_NAME} \
   -e MYSQL_ALLOW_EMPTY_PASSWORD=yes \
-  -d mysql:5.6
+  -v ${CONF_DIRECTORY}:/etc/mysql/conf.d \
+  -d \
+  mysql:5.6
 
 echo "mysql docker container started."
 echo "    mysql -u root -h 127.0.0.1 -P ${PORT_NUMBER} -A ${DATABASE_NAME}"
